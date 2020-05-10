40b5b4844a0ecdf5c0e29d1bce544b3cbfa254a8
cbi11 <58070870+cbi11@users.noreply.github.com>
2020-05-10 08:52:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+document.getElementById('burger').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '100%';
+    document.getElementById('mobile_menu').style.position = 'fixed'
+})
+document.getElementById('cls').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0'
+})
+
+
+
+document.getElementById('vsb1').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb2').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb3').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb4').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb5').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb6').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb7').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb8').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb9').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb10').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb11').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+document.getElementById('vsb12').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0';
+})
+
+
